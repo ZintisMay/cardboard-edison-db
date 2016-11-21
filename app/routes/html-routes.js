ef3a81bacf9c8df4401a01dd3cc970b1e11a572f
ZintisMay <zbmayk@gmail.com>
2016-11-21 01:23:21 -0500

    1st

---
+++ app/routes/html-routes.js
@@ -0,0 +1,27 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path 		= require('path');
+
+
+// Routes
+// =============================================================
+module.exports = function(app){
+
+	// Each of the below routes just handles the HTML page that the user gets sent to.
+	app.get('/', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+	});
+
+	app.get('/CEangularTable', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/CEangularTable.html'));
+	});
+
+	app.get('/CEangularNewCompany', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/CEangularNewCompany.html'));
+	});
+
+}
